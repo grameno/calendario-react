9be1d1a25c7442aa03b02cbd10a334bfa9efe1d8
grameno <grameno86@gmail.com>
2025-05-28 17:38:04 +0200

    Collego InteractiveCalendar ad App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import InteractiveCalendar from './InteractiveCalendar';
 
 function App() {
   return (
-    <div>
-      <InteractiveCalendar />
-    </div>
+    <InteractiveCalendar />
   );
 }
 
